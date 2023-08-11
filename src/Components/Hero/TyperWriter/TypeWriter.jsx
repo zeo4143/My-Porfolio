9dfb5aa08d7682987abee79dc339f12eb13d6d4a
Shaik Mahammad Rafi <117294084+zeo4143@users.noreply.github.com>
2023-08-11 20:33:03 +0530

    Update TypeWriter.jsx

--- src/Components/Hero/TyperWriter/TypeWriter.jsx
+++ src/Components/Hero/TyperWriter/TypeWriter.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import  { useState, useEffect } from 'react';
-import './Typewriter.css'; // Add your styles here
+import './TypeWriter.css'; // Add your styles here
 
 export default function Typewriter({ texts, typingDelay = 100, delayAfterTyping = 2000 }) {
     const [currentTextIndex, setCurrentTextIndex] = useState(0);
